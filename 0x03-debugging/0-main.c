124f1c10ed061cda8ac2b57210ef8b16d9a7a02d
Abiodun <alabiabiodunemmanuel@gmail.com>
2022-04-15 15:04:42 -0700

    New task

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - texts funtion that prints if interger is positive or negative
+ * Return: zero
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
